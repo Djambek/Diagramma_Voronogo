1afe0a146d80e537aa2c5fec0644832d8df84707
Dzambek <44769905+Dzambek@users.noreply.github.com>
2021-03-01 10:54:36 +0300

    Update perpendikular.py
    Правильная версия. Исправлены баги

--- perpendikular.py
+++ perpendikular.py
@@ -2,6 +2,9 @@
 def per(x1,y1, x2, y2):
     k = (y1-y2)/(x1-x2)
     b = y2 - k*x2
-    b_answer = abs(y2-y1)/2 + y1
     k_answer = -1/k
+    x_ser = (x1+x2)/2
+    y_ser = (y1+y2)/2
+    b_answer = k_answer*x_ser - y_ser
+
     return k_answer, b_answer
